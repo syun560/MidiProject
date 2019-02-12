79208f47b386549b93988cc57dfc8b6bbcc809ac
syun560 <bajikoji@gmail.com>
2019-02-12 14:39:02 +0900

    fwriteが謎

--- MainScene.cpp
+++ MainScene.cpp
@@ -5,34 +5,31 @@
 #include "stdlib.h"
 
 MainScene::MainScene() {
-	filename = "sample.mid";
 	int check = 1;
 	if (*(char *)&check) IsLittleEndian = true;
 	else IsLittleEndian = false;
 }
 
-// デルタタイムをバイナリ可変表現に変換する
-int Delta(int delta) {
-	return 0;
-}
-
-void MainScene::ConvertEndian(UIntInByte input, size_t s) {
-	char *temp;
-	temp = new char[s];
+void MainScene::ConvertEndian(UIntInByte& input, size_t s) {
+	char *tmp;
+	tmp = new char[s];
 	for (int i = 0; i < s; ++i) {
-		temp[i] = input.c[i];
+		tmp[i] = input.c[i];
 	}
 	for (int i = 1; i <= s; ++i) {
-		input.c[i - 1] = temp[s - i];
+		input.c[i - 1] = tmp[s - i];
 	}
-	delete[] temp;
+	delete[] tmp;
 }
 
 void MainScene::MidiRead(){
-	if ((fp = fopen(filename, "rb")) == NULL) {
+	FILE* fp;
+	if ((fp = fopen("default.mid", "rb")) == NULL) {
 		printfDx("ファイルのオープンに失敗しました。\n");
 		return;
 	}
+
+	// ヘッダチャンク読み取り
     fread(&header.type, 4, 1, fp);
     fread(&header.size, 4, 1, fp);
     fread(&header.format, 2, 1, fp);
@@ -40,7 +37,6 @@ void MainScene::MidiRead(){
     fread(&header.division, 2, 1, fp);
 
     if (IsLittleEndian) {
-		ConvertEndian(header.type, 4);
         ConvertEndian(header.size, 4);
         ConvertEndian(header.format, 2);
         ConvertEndian(header.track, 2);
@@ -48,66 +44,64 @@ void MainScene::MidiRead(){
     }
 
     printfDx("ヘッダチャンク情報\n"); 
-    printfDx("タイプ : %c%c%c%c\n", header.type.c[0], header.type.c[1], header.type.c[2], header.type.c[3]);
-    printfDx("サイズ : %d\n", header.size.i);
-    printfDx("フォーマット : %hd\n", header.format.i);
-    printfDx("トラック数 : %hd\n", header.track.i);
-    printfDx("分解能 : %hd\n", header.division.i);
+    printfDx("タイプ:%c%c%c%c", header.type.c[0], header.type.c[1], header.type.c[2], header.type.c[3]);
+    printfDx(", サイズ:%d", header.size.i);
+    printfDx(", フォーマット:%d", header.format.i);
+    printfDx(", トラック数:%d", header.track.i);
+    printfDx(", 分解能:%d\n", header.division.i);
 
-
+	// トラックチャンク読み取り
 	track = new TrackChunk[header.track.i];
     for(int i = 0; i < header.track.i; ++i) {
         fread(&track[i].type, 4, 1, fp);
-        fread(&track[i].size.i, 4, 1, fp);
+        fread(&track[i].size, 4, 1, fp);
 		if (IsLittleEndian) {
-			ConvertEndian(track[i].type, 4);
 			ConvertEndian(track[i].size, 4);
 		}
 		track[i].data = new char[track[i].size.i];
         fread(track[i].data, track[i].size.i, sizeof(char), fp);
     }
 
-    // 読み取ったトラックチャンク情報を出力
-    for(int i = 0; i < header.track.i; ++i){   // トラック数だけ繰返し
-        printfDx("トラック[%02d]\n", i);
-        printfDx("トラック[%d].type : %c%c%c%c\n", i, track[i].type.c[0], track[i].type.c[1], track[i].type.c[2], track[i].type.c[3]);
-        printfDx("トラック[%d].size : %d\n", i, track[i].size.i);
-        printfDx("トラック[%d].data : \n", i);
+    for(int i = 0; i < header.track.i; ++i){
+        printfDx("トラック[%02d]", i);
+        printfDx(", タイプ:%c%c%c%c", track[i].type.c[0], track[i].type.c[1], track[i].type.c[2], track[i].type.c[3]);
+        printfDx(", サイズ:%d", track[i].size.i);
+        printfDx(", トラック[%d].data : \n", i);
         for(int j = 0; j < track[i].size.i; ++j){
-            printfDx("%02x ", (unsigned char)track[i].data[j]);   // 16進表記で出力
-            if(!((j+1)%10)) printf("\n : ");   // 10バイト出力するたびに改行
+            printfDx("%02x ", (unsigned char)track[i].data[j]);
+            if(!((j+1)%10)) printf("\n : ");
         }
         printfDx("\n");
     }
 	fclose(fp);
 }
 
-// 入力する関数
-int MainScene::Write(UIntInByte input, int size) {
-	ConvertEndian(input, size);
-	fwrite(input.c, size, 1, fp);
-	return 0;
-}
-
 void MainScene::MidiWrite() {
-	if ((fp = fopen(filename, "wb")) == NULL) {
+	FILE* fp;
+	if ((fp = fopen("output2.mid", "wb")) == NULL) {
 		printfDx("ファイルのオープンに失敗しました。\n");
 		return;
 	}
 	
-	//// ヘッダチャンク
-	//UIntInByte ctype, csize, format, track, division;
-	//ctype.i = 0x4d546864; //MThd
-	//csize.i = 6;
-	//format.i = 1;
-	//track.i = 2;
-	//division.i = 480;
-	//Write(ctype, 4);
-	//Write(csize, 4);
-	//Write(format, 2);
-	//Write(track, 2);
-	//Write(division, 2);
-	//
+	// ヘッダチャンク書き込み
+	fwrite(&header.type, 4, 1, fp);
+	fwrite(&header.size, 4, 1, fp);
+	fwrite(&header.format, 2, 1, fp);
+	fwrite(&header.track, 2, 1, fp);
+	fwrite(&header.division, 2, 1, fp);
+	printfDx("header.size = %d", header.size.i);
+
+	// トラックチャンク書き込み
+	for (int i = 0; i < header.track.i; ++i) {
+		fwrite(&track[i].type, 4, 1, fp);
+		if (IsLittleEndian) {
+			ConvertEndian(track[i].size, 4);
+		}
+		fwrite(&track[i].size, 4, 1, fp);
+		printfDx("track[%d].size = %d", i, track[i].size.i);
+		fwrite(track[i].data, 1, track[i].size.i, fp);
+	}
+	
 	//// トラックチャンク（Conductor）
 	//UIntInByte ttype, tsize, tempo, beat, beat2;
 	//ttype.i = 0x4d54726b; // MTrk
@@ -150,7 +144,7 @@ void MainScene::MidiWrite() {
 	//Write(&EOT, 4);
 	//fclose(fp);
 
-	//printfDx("%sを保存\n", filename);
+	printfDx("%sを保存\n", "output2.mid");
 	fclose(fp);
 }
 
@@ -162,15 +156,19 @@ void MainScene::Update() {
 		MidiWrite();
 	}
 	if (Input::Key(KEY_INPUT_A) == 1) {
-		PlayMusic(filename, DX_PLAYTYPE_BACK);
-		printfDx("%sを再生\n", filename);
+		PlayMusic("output.mid", DX_PLAYTYPE_BACK);
+		printfDx("%sを再生\n", "output.midS");
+	}
+	if (Input::Key(KEY_INPUT_C) == 1) {
+		clsDx();
 	}
 }
 
 void MainScene::Draw(){
 	if (IsLittleEndian) DrawFormatString(180, 0, WHITE, "リトルエンディアンです。");
-	else DrawFormatString(180, 0, WHITE, "ビッグエンディアンです。");
-	DrawFormatString(180, 100, WHITE, "Oキーで読込");
-	DrawFormatString(180, 120, WHITE, "Sキーで保存");
-	DrawFormatString(180, 140, WHITE, "Aキーで再生");
+	else DrawFormatString(200, 0, WHITE, "ビッグエンディアンです。");
+	DrawFormatString(200, 100, WHITE, "Oキーで読込");
+	DrawFormatString(200, 120, WHITE, "Sキーで保存");
+	DrawFormatString(200, 140, WHITE, "Aキーで再生");
+	DrawFormatString(200, 160, WHITE, "Cキーでクリア");
 }
--- MainScene.h
+++ MainScene.h
@@ -16,16 +16,13 @@ struct TrackChunk {
 };
 
 class MainScene {
-	FILE* fp;
-	char* filename;
 	HeaderChunk header;
 	TrackChunk* track;
 	bool IsLittleEndian;
 
 	void MidiRead();
-	int Write(UIntInByte input, int size);
 	void MidiWrite();
-	void ConvertEndian(UIntInByte input, size_t s);
+	void ConvertEndian(UIntInByte& input, size_t s);
 public:
 	MainScene();
 	void Update();
